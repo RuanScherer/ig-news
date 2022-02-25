fdf47d1ee4a83578ca622a3742da8e9f6c5e8328
Ruan Scherer <ruan.scherer@outlook.com>
2022-02-25 15:39:44 -0300

    create project with initial structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+      <h1>Hello, <span>World!</span></h1>
+    </>
+  )
+}
