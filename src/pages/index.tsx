70091bea6e4b13b0ee07f329945be677e1698da3
Ruan Scherer <ruan.scherer@outlook.com>
2022-02-26 14:55:47 -0300

    feat: create initial home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
@@ -6,7 +7,19 @@ export default function Home() {
       <Head>
         <title>ig.news</title>
       </Head>
-      <h1>Hello, <span>World!</span></h1>
+      
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br/>
+            <span>for $9.99 month</span>
+          </p>
+        </section>
+        
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
